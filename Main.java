9e4ea44a0771f8dfafb7434dab4e1e54f0f24cbe
AspiranteAudaz <lucas.moura.carvalho@usp.br>
2019-10-16 19:50:31 -0300

    Main adicionado de facto

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Sistema     sys = new Sistema("./Entrada", "./Saida", "quantum.txt", "prioridades.txt");
+        Escalonador esc = new Escalonador(sys);
 
+        esc.Escalona();
+        sys.GravaLog();
     }
 }
